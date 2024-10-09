592c4ddd979bd8aa8c5ee91af1749ea258c8484f
Shirley <shirleyzhu@college.harvard.edu>
2024-10-09 15:27:11 -0400

    Initial Commit

---
+++ MakeupForYou/MakeupForYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeupForYouApp.swift
+//  MakeupForYou
+//
+//  Created by ZhuMacPro on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MakeupForYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
