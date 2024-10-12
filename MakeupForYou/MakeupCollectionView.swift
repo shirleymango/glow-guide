5fb118dc69a97cda146f9413d1b0e33e4c3b7f53
Shirley <shirleyzhu@college.harvard.edu>
2024-10-12 15:20:36 -0400

    Change background to pink

--- MakeupForYou/MakeupCollectionView.swift
+++ MakeupForYou/MakeupCollectionView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct MakeupCollectionView: View {
+    let lightPink : Color = Color(red: 250/255, green: 182/255, blue: 206/255)
+    
     // Sample data for the collection view
     let makeupItems = [
         ["1", "Mattifying Waterproof Setting Spray"],
@@ -24,24 +26,27 @@ struct MakeupCollectionView: View {
     ]
 
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: columns, spacing: 20) {
-                ForEach(makeupItems, id: \.self) { item in
-                    VStack {
-                        Image(item[0])
-                            .resizable()
-                            .scaledToFit()
-                            .frame(height: 100)
-                            .cornerRadius(8)
-                        Text(item[1])
-                            .font(.headline)
-                            .multilineTextAlignment(.center)
-                            .padding(.top, 5)
+        ZStack {
+            lightPink.ignoresSafeArea()
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(makeupItems, id: \.self) { item in
+                        VStack {
+                            Image(item[0])
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 100)
+                                .cornerRadius(8)
+                            Text(item[1])
+                                .font(.headline)
+                                .multilineTextAlignment(.center)
+                                .padding(.top, 5)
+                                .foregroundColor(.black)
+                        }
                     }
                 }
+                .padding()
             }
-            .padding()
-            .navigationTitle("Your Collection")
         }
     }
 }
--- MakeupForYou/SelfieCaptureView.swift
+++ MakeupForYou/SelfieCaptureView.swift
@@ -1,100 +1,104 @@
 import SwiftUI
 
 struct SelfieCaptureView: View {
+    let lightPink : Color = Color(red: 250/255, green: 182/255, blue: 206/255)
+    let hotPink : Color = Color(red: 242/255, green: 12/255, blue: 93/255)
+    
     @State private var capturedImage: UIImage? = nil
     @State private var showLoadingView = false
     @State private var isPhotoTaken = false
 
-    // Custom pastel colors
-    let customMint = Color(red: 189 / 255, green: 252 / 255, blue: 201 / 255)
-    let customPastelPink = Color(red: 255 / 255, green: 182 / 255, blue: 193 / 255)
-
     var body: some View {
-        VStack {
-            Text("Take a Selfie!")
-                .font(.title)
-                .padding()
-
-            if let image = capturedImage {
-                // Display the captured image in place of the camera feed
-                Image(uiImage: image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height: 400)
-                    .cornerRadius(12)
+        ZStack {
+            lightPink
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Take a Selfie!")
+                    .font(.title)
                     .padding()
+                    .foregroundColor(.black)
 
-                HStack(spacing: 20) {
-                    Button(action: {
-                        // Retake the photo by resetting the captured image
-                        capturedImage = nil // Show the camera feed again
-                    }) {
-                        Text("Retake Photo")
-                            .padding()
-                            .background(customPastelPink.opacity(0.8)) // Soft pastel pink background
-                            .foregroundColor(.white)
-                            .cornerRadius(8)
-                    }
+                if let image = capturedImage {
+                    // Display the captured image in place of the camera feed
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 400)
+                        .cornerRadius(12)
+                        .padding()
 
-                    NavigationLink(
-                        destination: LoadingView(),
-                        isActive: $showLoadingView,
-                        label: {
-                            Button(action: {
-                                // Show the loading view and then transition to the makeup collection view
-                                showLoadingView = true
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                                    isPhotoTaken = true
-                                }
-                            }) {
-                                Text("Use Photo")
-                                    .padding()
-                                    .background(customMint.opacity(0.8)) // Soft custom mint green background
-                                    .foregroundColor(.white)
-                                    .cornerRadius(8)
-                            }
+                    HStack(spacing: 20) {
+                        Button(action: {
+                            // Retake the photo by resetting the captured image
+                            capturedImage = nil // Show the camera feed again
+                        }) {
+                            Text("Retake Photo")
+                                .padding()
+                                .background(hotPink) // Soft pastel pink background
+                                .foregroundColor(.white)
+                                .cornerRadius(8)
                         }
-                    )
-                }
-                .padding(.top, 20)
-            } else {
-                // Show the live camera feed when no photo is taken
-                CameraView(capturedImage: $capturedImage)
-                    .frame(height: 400)
-                    .cornerRadius(12)
-                    .padding()
 
-                Spacer()
+                        NavigationLink(
+                            destination: LoadingView(),
+                            isActive: $showLoadingView,
+                            label: {
+                                Button(action: {
+                                    // Show the loading view and then transition to the makeup collection view
+                                    showLoadingView = true
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                        isPhotoTaken = true
+                                    }
+                                }) {
+                                    Text("Use Photo")
+                                        .padding()
+                                        .background(Color.green) // Soft custom mint green background
+                                        .foregroundColor(.white)
+                                        .cornerRadius(8)
+                                }
+                            }
+                        )
+                    }
+                    .padding(.top, 20)
+                } else {
+                    // Show the live camera feed when no photo is taken
+                    CameraView(capturedImage: $capturedImage)
+                        .frame(height: 400)
+                        .cornerRadius(12)
+                        .padding()
 
-                Button(action: {
-                    // Capture photo functionality triggered when button is tapped
-                    NotificationCenter.default.post(name: NSNotification.Name("capturePhoto"), object: nil)
-                }) {
-                    Circle()
-                        .fill(Color.white)
-                        .frame(width: 70, height: 70)
-                        .overlay(
-                            Circle()
-                                .stroke(customPastelPink, lineWidth: 4) // Pink border for a cute touch
-                        )
-                        .overlay(
-                            Image(systemName: "camera.fill")
-                                .foregroundColor(customPastelPink) // Pink camera icon
-                                .font(.system(size: 24))
-                        )
-                        .shadow(radius: 5)
+                    Spacer()
+
+                    Button(action: {
+                        // Capture photo functionality triggered when button is tapped
+                        NotificationCenter.default.post(name: NSNotification.Name("capturePhoto"), object: nil)
+                    }) {
+                        Circle()
+                            .fill(Color.white)
+                            .frame(width: 70, height: 70)
+                            .overlay(
+                                Circle()
+                                    .stroke(hotPink) // Pink border for a cute touch
+                            )
+                            .overlay(
+                                Image(systemName: "camera.fill")
+                                    .foregroundColor(hotPink) // Pink camera icon
+                                    .font(.system(size: 24))
+                            )
+                            .shadow(radius: 5)
+                    }
+                    .padding(.bottom, 40)
                 }
-                .padding(.bottom, 40)
             }
-        }
-        .padding()
-        .background(
-            NavigationLink(
-                destination: MakeupCollectionView(),
-                isActive: $isPhotoTaken,
-                label: { EmptyView() }
+            .padding()
+            .background(
+                NavigationLink(
+                    destination: MakeupCollectionView(),
+                    isActive: $isPhotoTaken,
+                    label: { EmptyView() }
+                )
             )
-        )
+        }
     }
 }
 
